155e017c5b2af29b01aac3806fa0252a7f6a1a8e
perfdawg <d6zhjrc5dz@privaterelay.appleid.com>
2026-05-10 17:15:23 +0530

    ts-v1: orderbook order endpoint init

--- index.ts
+++ index.ts
@@ -1,1 +1,45 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+import bodyParser from "body-parser";
+
+export const app = express();
+
+app.use(bodyParser({}));
+
+interface Balances {
+  [key: string]: number;
+}
+
+interface User {
+  id: string;
+  balances: Balances;
+}
+
+interface Order {
+  userId: string;
+  price: number;
+  quantity: number;
+}
+
+export const TICKER = "GOOGLE";
+
+const users: User[] = [
+  {
+    id: "1",
+    balances: {
+      GOOGLE: 10,
+      USD: 50000,
+    },
+  },
+  {
+    id: "2",
+    balances: {
+      GOOGLE: 10,
+      USD: 50000,
+    },
+  },
+];
+
+const bids: Order[] = [];
+const asks: Order[] = [];
+
+app.post("/order", (req: Request, res: Response) => {});
