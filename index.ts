036516465d92a8133739190624b3e692ad5c9029
perfdawg <d6zhjrc5dz@privaterelay.appleid.com>
2026-05-10 19:31:19 +0530

    ts-v1: fillorders and flipbalance fns

--- index.ts
+++ index.ts
@@ -42,4 +42,67 @@ const users: User[] = [
 const bids: Order[] = [];
 const asks: Order[] = [];
 
-app.post("/order", (req: Request, res: Response) => {});
+app.post("/order", (req: Request, res: Response) => {
+  const { side, price, quantity, userId } = req.body;
+});
+
+function flipBalance(
+  userId1: string,
+  userId2: string,
+  quantity: number,
+  price: number,
+) {
+  let user1 = users.find((x) => x.id == userId1);
+  let user2 = users.find((x) => x.id == userId2);
+  if (!user1 || !user2) {
+    return;
+  }
+
+  user1.balances[TICKER]! -= quantity;
+  user2.balances[TICKER]! += quantity;
+  user1.balances["USD"]! += quantity * price;
+  user2.balances["USD"]! -= quantity * price;
+}
+
+function fillOrders(
+  side: string,
+  price: number,
+  quantity: number,
+  userId: string,
+): number {
+  let remainingQuantity = quantity;
+  if (side == "bid") {
+    for (let i = asks.length - 1; i >= 0; i--) {
+      if (asks[i]?.price! > price) {
+        continue;
+      }
+
+      if (asks[i]?.quantity! > remainingQuantity) {
+        asks[i].quantity! -= remainingQuantity;
+        flipBalance(asks[i].userId, userId, remainingQuantity, asks[i].price);
+        return 0;
+      } else {
+        remainingQuantity -= asks[i].quantity;
+        flipBalance(asks[i].userId, userId, asks[i].quantity, asks[i].price);
+        asks.pop();
+      }
+    }
+  } else {
+    for (let i = bids.length - 1; i >= 0; i--) {
+      if (bids[i].price < price) {
+        continue;
+      }
+      if (bids[i].quantity > remainingQuantity) {
+        bids[i].quantity -= remainingQuantity;
+        flipBalance(userId, bids[i].userId, remainingQuantity, price);
+        return 0;
+      } else {
+        remainingQuantity -= bids[i].quantity;
+        flipBalance(userId, bids[i].userId, bids[i].quantity, price);
+        bids.pop();
+      }
+    }
+  }
+
+  return remainingQuantity;
+}
